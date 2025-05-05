d6f427b85acebe7f3080060c0b98a4d732a21954
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-05-05 10:27:48 +0300

    First Commit

---
+++ FirstApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derya Karahan");
+            Console.WriteLine("Yazılım alanındaki bilgi ve deneyimimi artırmak, Patika'nın uzman kadrosunun desteğiyle kendimi geliştirmek ve kariyerime iyi bir şirkette devam edebilmek amacıyla, bu programa her yönden katkı sağlayacağına inanarak katıldım.");
+        }
+    }
+}
