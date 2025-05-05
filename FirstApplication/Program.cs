19c5ab5c902e2df094b9df0e1aa59f353c549f29
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-05-05 10:41:59 +0300

    name updated

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -6,7 +6,7 @@ namespace FirstApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Derya Karahans");
+            Console.WriteLine("Derya Karahan");
             Console.WriteLine("Yazılım alanındaki bilgi ve deneyimimi artırmak, Patika'nın uzman kadrosunun desteğiyle kendimi geliştirmek ve kariyerime iyi bir şirkette devam edebilmek amacıyla, bu programa her yönden katkı sağlayacağına inanarak katıldım.");
         }
     }
